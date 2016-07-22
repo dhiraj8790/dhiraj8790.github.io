ffd9016eae13ff0b3a19ed4c345aff16aa0637f7
dhiraj8790 <dhiraj8790@gmail.com>
2016-07-22 12:58:03 +0530

    Initial commit

---
+++ amazon.php
@@ -0,0 +1,10 @@
+<div style="text-align: center;">
+<script type="text/javascript" language="javascript"> var aax_size='728x90'; var aax_pubname = 'dhiraj8790git-21'; var aax_src='302'; </script><script type="text/javascript" language="javascript" src="http://c.amazon-adsystem.com/aax2/assoc.js"></script>
+</div>
+
+<SCRIPT charset="utf-8" type="text/javascript" src="http://ws-in.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=IN&ID=V20070822%2FIN%2Fdhiraj8790git-21%2F8003%2F9c767f7d-dd19-4c4d-bfa6-7d38dd403320&Operation=GetScriptTemplate"> </SCRIPT> <NOSCRIPT><A rel="nofollow" HREF="http://ws-in.amazon-adsystem.com/widgets/q?rt=tf_ssw&ServiceVersion=20070822&MarketPlace=IN&ID=V20070822%2FIN%2Fdhiraj8790git-21%2F8003%2F9c767f7d-dd19-4c4d-bfa6-7d38dd403320&Operation=NoScript">Amazon.in Widgets</A></NOSCRIPT>
+
+<?php 
+
+
+?>
